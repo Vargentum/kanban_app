44912d53ccba0eb6f129a3f24810a46b10c691e0
Vargentum <vargentum@gmail.com>
2015-07-23 14:51:23 +0300

    05. webpack and react

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var path = require('path');
 var merge = require('webpack-merge');
 var HtmlwebpackPlugin = require('html-webpack-plugin')
@@ -7,6 +8,9 @@ var ROOT_PATH = path.resolve(__dirname);
 
 var common = {
   entry: [path.resolve(ROOT_PATH, 'app/main')],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: path.resolve(ROOT_PATH, 'build'),
     filename: 'bundle.js'
@@ -26,11 +30,49 @@ var common = {
   ]
 };
 
-// we'll extend these later and use merge then
+// npm run build
 if(TARGET === 'build') {
-  module.exports = common;
+  module.exports = merge(common, {
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/, //both js & jsx
+          loader: 'babel?stage=1', //include with Stage 1 features
+          include: path.resolve(ROOT_PATH, 'app') // operate only inside ./app folder
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production') //affected react lib size
+        }
+      })
+    ]
+  });
 }
 
+// npm start
 if(TARGET === 'dev') {
-  module.exports = common;
+  module.exports = merge(common, {
+    entry: [
+      'webpack-dev-server/client?http://0.0.0.0:9094',
+      'webpack/hot/dev-server'
+    ],
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          loaders: ['react-hot', 'babel?stage=1'],
+          include: path.resolve(ROOT_PATH, 'app')
+        }
+      ]
+    }
+  });
 }
